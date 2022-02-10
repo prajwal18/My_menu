6f4589423433020e6f9327cd8e1667c69a8ba96f
Prajwal Gautam <prajwalgautam10@gmail.com>
2022-02-11 00:51:20 +0545

    Version 1 c

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
