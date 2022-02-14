9fa8420d46f351cabff5c0e216c134c4c64b986b
Prajwal Gautam <prajwalgautam10@gmail.com>
2022-02-14 20:51:47 +0545

    Version 1 d router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './menu.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
